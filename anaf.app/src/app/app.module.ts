bb9e670dad4c48db5221eaa5a7d4c6ee118fd257
Alexandru Chiraples <alex.chiraples@gmail.com>
2017-01-08 20:49:33 +0200

    Revert "Rss service & Rss component" (#14)

--- anaf.app/src/app/app.module.ts
+++ anaf.app/src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { HomePage, AboutPage, ContactPage, AsistentaPage, RssComponent, RssFeedComponent, RssFeedItemComponent  } from './pages/index';
-import { RssService } from './providers/rss.service';
-import { RSS_CHANNEL_LIST_TOKEN, RSS_CHANNEL_LIST } from './shared/config';
+import { HomePage, AboutPage, ContactPage,AsistentaPage } from './pages/index';
 
 @NgModule({
     declarations: [
@@ -11,20 +9,13 @@ import { RSS_CHANNEL_LIST_TOKEN, RSS_CHANNEL_LIST } from './shared/config';
         HomePage,
         AboutPage,
         ContactPage,
-        AsistentaPage,
-        RssComponent,
-        RssFeedComponent,
-        RssFeedItemComponent
+        AsistentaPage
     ],
-    imports: [
+    imports: [ 
         IonicModule.forRoot(MyApp)
     ],
     bootstrap: [IonicApp],
-    providers: [
-        { provide: ErrorHandler, useClass: IonicErrorHandler },
-        RssService,
-        { provide: RSS_CHANNEL_LIST_TOKEN, useValue: RSS_CHANNEL_LIST } //required for RSS Channel List configuration
-    ],
+    providers: [{ provide: ErrorHandler, useClass: IonicErrorHandler }],
     entryComponents: [
         HomePage,
         AboutPage,
--- anaf.app/src/app/pages/index.ts
+++ anaf.app/src/app/pages/index.ts
@@ -2,6 +2,3 @@
 export * from './contact/contact.component';
 export * from './home/home.component';
 export * from './asistenta/asistenta.component';
-export * from './rss/rss.component';
-export * from './rss-feed/rss-feed.component';
-export * from './rss-feed-item/rss-feed-item.component';
