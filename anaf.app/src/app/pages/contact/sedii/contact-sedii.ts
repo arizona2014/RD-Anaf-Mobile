73d465b305a6c6cb8a935102e5c38309d85272b6
arizona2014 <andy.lisac@gmail.com>
2017-01-15 18:41:32 +0100

    Modified contact sedii page for sedii harta page

--- anaf.app/src/app/pages/contact/sedii/contact-sedii.ts
+++ anaf.app/src/app/pages/contact/sedii/contact-sedii.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {SediiHartaPage} from "./harta/sedii-harta";
 
 @Component({
   selector: 'page-contact-sedii',
@@ -7,10 +8,23 @@ import { NavController } from 'ionic-angular';
 })
 export class ContactSediiPage {
 
-  constructor(public navCtrl: NavController) {}
+  sedii: any;
+
+  constructor(public navCtrl: NavController) {
+    this.sedii = [
+      {titlu: "Anaf Sector 1", adresa: "Blv. Magheru, Bucuresti", telefon: "0123456789", distanta: "3.5", coords: "100.3, 11.4"},
+      {titlu: "Anaf Sector 2", adresa: "Piata Romana, Bucuresti", telefon: "3216549870", distanta: "9.5", coords: "200.3, 21.4"},
+      {titlu: "Anaf Sector 3", adresa: "Splaiul Independentei, Bucuresti", telefon: "9876543210", distanta: "12.5", coords: "300.3, 31.4"}
+    ];
+  }
 
   ionViewDidLoad() {
-    console.log('Hello ContactSediiPage Page');
+    //console.log('Hello ContactSediiPage Page');
+  }
+
+  vizualizare(sediu: any) {
+    //console.log(sediu);
+    this.navCtrl.push(SediiHartaPage, {coords: sediu.coords});
   }
 
 }
